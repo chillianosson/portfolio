54337ddcbe06f9a0682922b7dca820fd6bbc56a7
Cillian O Murchu <cillian.omurchu@betssongroup.com>
2022-01-16 02:04:33 +0100

    feat(text): improve it

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component, HostListener, OnInit } from "@angular/core";
 
+import { item1 } from "./const";
+
 @Component({
 	selector: 'app-blog',
 	templateUrl: './blog.component.html',
 	styleUrls: ['./blog.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BlogComponent implements OnInit{
+export class BlogComponent implements OnInit {
 
 	screenHeight = 0;
 	screenWidth = 0;
@@ -14,11 +16,11 @@ export class BlogComponent implements OnInit{
 
 	@HostListener('window:resize', ['$event'])
 	onResize() {
-		this.screenHeight = window.innerHeight;
+		this.screenHeight = window.innerHeight - 41;
 	}
 
 	items = [
-		"I build large scale applications primarily with Angular, in JavaScript, for desktop, tablet, mobile or server.Combined with a background in UX and design means I have now the ability to take applications from conception to completion autonomously."
+		item1
 	];
 
 	constructor() {
---
+++ src/app/components/blog/const.ts
@@ -0,0 +1,3 @@
+export const item1 = `I build large scale applications primarily with Angular, in JavaScript, for desktop, tablet and
+mobile with server.Combined with a background
+in UX and design means I have now the ability to take applications from conception to completion autonomously.`;
